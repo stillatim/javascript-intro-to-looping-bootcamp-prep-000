e5fb99c2330f9c41d5b96bc9d0a1ada92a6ca788
Justin Evans <evansjustindavid@gmail.com>
2020-02-28 21:23:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
+<<<<<<< HEAD
    if (i === 1) {
      array.push(`I am 1 strange loop.`)
     } else {
@@ -9,10 +10,20 @@ function forLoop(array) {
     return array; 
 }
 
+=======
+    if (i = 1) {array.push('I am 1 strange loop.')
+    } 
+    else {array.push('I am ${i} strange loops.')
+    }
+    return(array)
+  }
+  }
+>>>>>>> 0e17a4cd55665158b5d88e7be641e235cd855870
 
 function whileLoop(n) {
   while (n > 0) {
     console.log(n);
+<<<<<<< HEAD
     n--
     };  
   if (n === 0) {
@@ -24,12 +35,30 @@ var i = 0
 
 function incrementVariable() {
   i = i + 1;
+=======
+    n--}; 
+  if (n = 0) {
+    return('done')
+}
+}
+
+
+function incrementVariable() {
+  i = i + 1; 
+>>>>>>> 0e17a4cd55665158b5d88e7be641e235cd855870
   return i;
 }
 
 function doWhileLoop(n) {
   do {
+<<<<<<< HEAD
     console.log("I run once regardless."); 
   } while (incrementVariable() <= n);
 }
  
+=======
+    console.log('I run once regardless.'); 
+  } while (incrementVariable() < n)
+  }
+}
+>>>>>>> 0e17a4cd55665158b5d88e7be641e235cd855870
